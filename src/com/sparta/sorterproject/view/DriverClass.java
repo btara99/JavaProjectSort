a247c781e541225c583cb67c7bebd36ddafeea11
Bahir Taraki <btara001@gold.ac.uk>
2021-10-01 12:41:25 +0100

    added the exception and cleaned code

--- src/com/sparta/sorterproject/view/DriverClass.java
+++ src/com/sparta/sorterproject/view/DriverClass.java
@@ -14,9 +14,9 @@ public class DriverClass {
     public static void main(String[] args) {
         SortFactory sortFactory =  new SortFactory();
         DisplayManager displayData = new DisplayManager();
-        int status = 0;
+        boolean status = true;
 
-        while(status == 0) {
+        while(status) {
             System.out.println(displayData.displaySorts()); //Displays the menu for the sorts
             try {
                 Scanner scanner = new Scanner(in);
@@ -25,36 +25,35 @@ public class DriverClass {
                     case 0:
                         SortManager bubbleSortDisplay = sortFactory.getSort(0);
                         bubbleSortDisplay.displaySortedArr();
-                        status=1;
+                        status=false;
                         break;
 
                     case 1:
                         SortManager mergeSortDisplay = sortFactory.getSort(1);
                         mergeSortDisplay.displaySortedArr();
-                        status=1;
+                        status=false;
                         break;
 
                     case 2:
                         SortManager quickSortDisplay = sortFactory.getSort(2);
                         quickSortDisplay.displaySortedArr();
-                        status=1;
+                        status=false;
                         break;
 
                     case 3:
                         SortManager insertionSortDisplay = sortFactory.getSort(3);
                         insertionSortDisplay.displaySortedArr();
-                        status=1;
+                        status=false;
                         break;
 
                     case 4:
                         SortManager selectionSortDisplay = sortFactory.getSort(4);
                         selectionSortDisplay.displaySortedArr();
-                        status=1;
+                        status=false;
                         break;
 
                 default:
                     System.err.println("Incorrect number in range please select a number between 0-5");
-
                 }
 
             } catch (NumberFormatException n) {
